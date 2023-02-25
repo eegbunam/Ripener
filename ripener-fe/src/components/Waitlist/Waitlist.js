ed6265a03cbcc5c4d15dd6cff0c2bdb300e77784
egb_the_boss <ebukaegb1@gmail.com>
2023-02-25 17:19:50 -0600

    added some code to handle email

--- ripener-fe/src/components/Waitlist/Waitlist.js
+++ ripener-fe/src/components/Waitlist/Waitlist.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Lottie from "react-lottie";
 import animationData from "../../animations/Waitlist2.json";
 import "./Waitlist.css";
@@ -17,28 +17,56 @@ const Waitlist = () => {
     },
   };
 
+  // Declare state for form values
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Email:", email);
+    // do something with the email
+  };
+
+  // Handle email input change
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
   return (
     <div className="waitlist-screen">
       <div className="waitlist-content">
         <h1>Be the first to Join Ripener</h1>
         <p>The first product to comercialise contributions to open source!</p>
-
-        <div className="input-group">
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-            placeholder="eg:test@gmail.com"
-          />
-          <button type="submit">Join Waitlist</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Enter your email:</label>
+            <div className="input-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <button type="submit">Join Waitlist</button>
+            </div>
+          </div>
+        </form>
+        <div className="input-group"></div>
         <div className="animation-container">
           <Lottie options={animationOptions} height={400} width={400} />
         </div>
         <form>
           <div className="form-group"></div>
         </form>
+
+        <motion.div
+          animate={{ y: [0, -10, 0] }}
+          transition={{ repeat: Infinity, duration: 1 }}
+        >
+          <img src="https://assets.codepen.io/35984/star.svg" alt="star" />
+        </motion.div>
         <div className="additional-animations">
           <Confetti
             width={window.innerWidth}
